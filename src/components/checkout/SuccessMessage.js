ca140a724b2e5275179667f46e0fc223b3fe9b4a
Jan Schwarz <jan-paul@schw-a-rz.de>
2024-11-22 13:14:43 +0100

    style update

--- src/components/checkout/SuccessMessage.js
+++ src/components/checkout/SuccessMessage.js
@@ -23,20 +23,24 @@ export default function SuccessMessage({ totalAmount }) {
     <>
       <Modal className={`animte-in duration-300 zoom-in`}>
         <div
-          className={`m-6 flex flex-col gap-6 md:m-12 md:gap-12`}
+          className={`m-6 grid grid-cols-5 gap-6 md:m-12 md:gap-12`}
           onClick={(event) => event.stopPropagation()}
         >
           <TickSVG />
-          <h2 className={`fontPreset4 md:fontPreset2 text-primaryColor`}>
+          <h2
+            className={`fontPreset4 md:fontPreset2 col-span-5 text-primaryColor`}
+          >
             THANK YOU{" "}
             <span className={`block text-nowrap`}>FOR YOUR ORDER</span>
           </h2>
-          <p className={`fontPreset7 text-primaryColor text-opacity-60`}>
+          <p
+            className={`fontPreset7 col-span-5 text-primaryColor text-opacity-60`}
+          >
             You will receive an email confirmation shortly.
           </p>
-          <div className={`grid md:grid-cols-2`}>
+          <div className={`col-span-5 grid md:grid-cols-5`}>
             <div
-              className={`flex flex-col items-center justify-center gap-4 rounded-t-md bg-lightColor px-4 py-8 transition-all duration-500 md:gap-8 md:rounded-s-md`}
+              className={`col-span-5 flex flex-col items-center justify-center gap-4 rounded-t-md bg-lightColor px-4 py-8 transition-all duration-500 md:col-span-3 md:gap-8 md:rounded-s-md`}
             >
               {cart.map((carItem, index) => {
                 return (
@@ -70,7 +74,7 @@ export default function SuccessMessage({ totalAmount }) {
               )}
             </div>
             <div
-              className={`flex flex-col justify-end gap-4 rounded-b-md bg-primaryColor px-6 py-8 md:rounded-e-md`}
+              className={`col-span-5 flex flex-col justify-end gap-4 rounded-b-md bg-primaryColor px-6 py-8 md:col-span-2 md:rounded-e-md`}
             >
               <p
                 className={`fontPreset6 md:fontPreset5 text-secondaryColor text-opacity-70`}
@@ -84,7 +88,7 @@ export default function SuccessMessage({ totalAmount }) {
           </div>
           <LinkButton
             href={"/"}
-            className={`rounded-sm`}
+            className={`col-span-full rounded-sm`}
             primary
             highlighted
             text={"BACK TO HOME"}
