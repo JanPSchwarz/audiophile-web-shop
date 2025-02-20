5409d6bf217ee0de4794fe09a6853aaa6b10e243
Jan Schwarz <jan-paul@schw-a-rz.de>
2025-02-20 20:53:48 +0100

    try deployment error fix

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-import withPlaiceholder from "@plaiceholder/next";
 
 const nextConfig = {
   images: {
@@ -14,4 +13,4 @@ const nextConfig = {
   },
 };
 
-export default withPlaiceholder(nextConfig);
+export default nextConfig;
